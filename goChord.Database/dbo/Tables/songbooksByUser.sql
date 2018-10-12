f3adb6208c7c5de825d57877fc5a07f78f3fd48d
Luz Garcia <lagarciace@gmail.com>
2018-10-12 17:17:59 -0400

    creacion de indices unicos y autonumericos

--- goChord.Database/dbo/Tables/songbooksByUser.sql
+++ goChord.Database/dbo/Tables/songbooksByUser.sql
@@ -1,6 +1,11 @@
 ﻿CREATE TABLE [dbo].[songbookbyuser]
 (
-	[songbooksByUserId] INT NOT NULL PRIMARY KEY, 
+	[songbooksByUserId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [userId] INT NOT NULL, 
     [songbookId] INT NOT NULL
 )
+
+
+GO
+
+CREATE UNIQUE INDEX [UX_songbookbyuser_userId_songbookId] ON [dbo].[songbookbyuser] ([userId], [songbookId])
--- goChord.Database/dbo/Tables/songsbysongbook.sql
+++ goChord.Database/dbo/Tables/songsbysongbook.sql
@@ -1,6 +1,10 @@
 ﻿CREATE TABLE [dbo].[songbysongbook]
 (
-	[songsBySongbookId] INT NOT NULL PRIMARY KEY, 
+	[songsBySongbookId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [songbookId] INT NOT NULL, 
-    [songId] INT NOT NULL
+    [songId] INT NOT NULL 
 )
+
+GO
+
+CREATE UNIQUE INDEX [UX_songbysongbook_songbookId_songId] ON [dbo].[songbysongbook] ([songId], [songsBySongbookId])
