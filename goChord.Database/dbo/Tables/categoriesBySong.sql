9886e48ef51372b315ca7100e2bb518806afb12f
lagarciace <lagarciace@gmail.com>
2018-10-17 21:18:41 -0400

    Creacion de tablas category y categoriesBySong +chv (#2)

---
+++ goChord.Database/dbo/Tables/categoriesBySong.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[categoriesBySong]
+(
+	[categoriesBySongId] INT NOT NULL  IDENTITY, 
+    [songId] INT NOT NULL,
+	[categoryId] INT NOT NULL, 
+    CONSTRAINT [PK_categoriesBySong] PRIMARY KEY ([categoriesBySongId]), 
+    CONSTRAINT [FK_categoriesBySong_song] FOREIGN KEY ([songId]) REFERENCES [song]([songId]), 
+    CONSTRAINT [FK_categoriesBySong_category] FOREIGN KEY ([categoryId]) REFERENCES [category]([categoryId])
+)
